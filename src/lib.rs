6c8919090988f828aade0b80491c814d217e7de2
Clement RENAULT <crenault@e6r4p4.vp.42.fr>
2017-05-16 18:06:36 +0200

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(try_from)]
+
 extern crate url;
 extern crate tokio_core;
 extern crate tokio_proto;
@@ -6,9 +8,37 @@ pub mod connection;
 
 pub use connection::Connection;
 
+use std::convert::TryFrom;
+
+#[derive(Debug)]
+pub enum CommandType {
+    HeartbeatRequestCommand,
+    HeartbeatResponseCommand,
+
+    //
+}
+
+impl TryFrom<u32> for CommandType {
+    type Error = ();
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        use CommandType::*;
+        Ok(match value {
+            0x01 => HeartbeatRequestCommand,
+            0x02 => HeartbeatResponseCommand,
+            _ => return Err( () )
+        })
+    }
+}
+
+//
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
+        let x = CommandType::try_from(0x12).unwrap();
     }
 }
